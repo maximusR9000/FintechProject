17c4c4222b5f3b9bfd83393a43d8ba25a6006518
Satyajeet Mahato <mahato.satyajeet1@gmail.com>
2025-04-26 13:13:16 +0530

    Decision Tree Model and Random Forest Model Script added

--- LoanDetection.R
+++ LoanDetection.R
@@ -2,7 +2,7 @@
 install.packages(c("tidyverse", "caret", "rpart", "randomForest", "rpart.plot"))
 
 #Setting current working directory for easy accessing the .csv files
-setwd("/Users/maximus/Downloads/Notes/Fintech /Project/archive")
+setwd("/Users/maximus/Downloads/Notes/Fintech /Project/FintechProject/archive")
 
 
 #Loading libraries
@@ -24,6 +24,17 @@ trainIndex = createDataPartition(loanData$Loan_Status, p = 0.8, list = FALSE)
 trainData = loanData[trainIndex, ]
 testData = loanData[-trainIndex, ]
 
+
+#Decision Tree Model
 treeModel = rpart(Loan_Status ~ ., data = trainData, method = "class")
 
 rpart.plot(treeModel)
+
+treePrediction = predict(treeModel, testData, type = "class")
+confusionMatrix(treePrediction, as.factor(testData$Loan_Status))
+
+#Random Forest Model
+set.seed(123)
+rfModel = randomForest(as.factor(Loan_Status) ~ ., data = trainData, ntree = 100)
+rfPrediction = predict(rfModel, testData)
+confusionMatrix(rfPrediction, as.factor(testData$Loan_Status))
